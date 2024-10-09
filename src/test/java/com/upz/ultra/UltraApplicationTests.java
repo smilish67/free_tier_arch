170d3fcb47fa392a614d58ccafe356b51138a718
UPZ <smilish67@naver.com>
2024-10-09 21:58:16 +0900

    first commit

---
+++ src/test/java/com/upz/ultra/UltraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upz.ultra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
